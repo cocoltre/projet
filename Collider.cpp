29ef7328f94c4784b74dc253536e4560e5e2f917
cocoltre <102475530+cocoltre@users.noreply.github.com>
2022-04-11 21:43:59 +0200

    Add files via upload

--- Collider.cpp
+++ Collider.cpp
@@ -10,13 +10,13 @@
 #include <algorithm>
 #include <array>
 
-Collider::Collider(Vec2d center, double arg_radius) : radius(arg_radius) { // constructeur
+Collider::Collider (Vec2d center, double arg_radius) : radius(arg_radius) { // constructeur
     this->center = clamp(center);
 }
 
-Collider::Collider (Collider const& autre) : center(autre.center), radius(autre.radius) {} // constructeur de copie
+Collider::Collider (Collider const& other) : center(other.center), radius(other.radius) {} // constructeur de copie
 
-Vec2d Collider::clamp (const Vec2d& vec) {
+Vec2d Collider::clamp (const Vec2d& vec) { // réajuste les coordonnées d'un vecteur dans le monde torique
     double x (0.00);
     double y (0.00);
 
@@ -32,8 +32,8 @@ Vec2d Collider::clamp (const Vec2d& vec) {
     return {x,y};
 }
 
- Vec2d Collider::directionTo (const Vec2d& to) const {
-    Vec2d to2 (to.x(), to.y()+world_height);
+ Vec2d Collider::directionTo (const Vec2d& to) const {      // calcule les coordonnées du vecteur de déplacement à partir d'un vecteur
+    Vec2d to2 (to.x(), to.y()+world_height);                // les 9 possibilités de déplacement
     Vec2d to3 (to.x(), to.y()-world_height);
     Vec2d to4 (to.x()+world_width, to.y());
     Vec2d to5 (to.x()-world_width, to.y());
@@ -44,7 +44,7 @@ Vec2d Collider::clamp (const Vec2d& vec) {
 
     std::array < Vec2d, 9 > L ({to,to2,to3,to4,to5,to6,to7,to8,to9});
 
-    double smallest_dist (distance(center, to));
+    double smallest_dist (distance(center, to));            // calcule le vecteur qui nécessitera le plus petit déplacement
     Vec2d good_vec(to);
     for (int i(1); i<9; ++i) {
         if (distance(center, L[i]) < smallest_dist) {
@@ -57,69 +57,69 @@ Vec2d Collider::clamp (const Vec2d& vec) {
 
 }
 
-Vec2d Collider::directionTo (const Collider& to) const {
+Vec2d Collider::directionTo (const Collider& to) const { // calcule les coordonnées du vecteur de déplacement à partir d'un Collider
     return directionTo(to.center);
 
 }
 
-double Collider::distanceTo (const Vec2d& to) const {
+double Collider::distanceTo (const Vec2d& to) const { // calcule la longueur du déplacement à partir d'un vecteur
     return directionTo(to).length();
 }
 
-double Collider::distanceTo (const Collider& to) const {
+double Collider::distanceTo (const Collider& to) const { // calcule la longueur du déplacement à partir d'un Collider
     return directionTo(to).length();
 }
 
-void Collider::move (const Vec2d& dx) {
-    this->center = clamp(this->center + dx);
+void Collider::move (const Vec2d& dx) {         // modifie la position du centre d'un Collider à partir d'un vecteur
+    this->center = clamp(this->center + dx);    // réajuste ses coordonnées dans le monde torique
 }
 
-void Collider::operator+= (const Vec2d& position2) {
+void Collider::operator+= (const Vec2d& position2) { // opérateur +=, fonction similaire à la méthode move
     move(position2);
 }
 
-bool Collider::isColliderInside (const Collider& other) {
+bool Collider::isColliderInside (const Collider& other) { // détermine si un Collider est à l'intérieur d'un autre
     if ((radius >= other.radius) and (distanceTo(other) <= radius - other.radius)) {
         return true;
     }
     else return false;
 }
 
-bool Collider::isColliding (const Collider& other) {
+bool Collider::isColliding (const Collider& other) { // détermine s'il y a collision entre deux Colliders
     if (distanceTo(other) <= other.radius + radius) {
         return true;
     }
     else return false;
 }
 
-bool Collider::isPointInside (const Vec2d& point) {
+bool Collider::isPointInside (const Vec2d& point) { // détermine si un point est à l'intérieur d'un Collider
     if (distanceTo(point) <= radius) {
         return true;
     }
     else return false;
 }
 
-bool Collider::operator> (const Collider& body2) {
+bool Collider::operator> (const Collider& body2) { // détermine si un Collider est à l'intérieur d'un autre
     return (isColliderInside(body2));
 }
 
-bool Collider::operator| (const Collider& body2) {
+bool Collider::operator| (const Collider& body2) { // détermine si deux Colliders sont en collision
     return (isColliding(body2));
 }
 
-bool Collider::operator> (const Vec2d& point) {
+bool Collider::operator> (const Vec2d& point) { // détermine si un point est à l'intérieur d'un Collider
     return (isPointInside(point));
 }
 
-Vec2d Collider::getPosition() const {
+Vec2d Collider::getPosition() const { // renvoie la position du centre d'un Collider
     return this->center;
 }
 
-double Collider::getRadius() const {
+double Collider::getRadius() const { // renvoie le rayon d'un Collider
     return radius;
 }
 
-std::ostream& operator<< (std::ostream& sortie, Collider const& body) {
+std::ostream& operator<< (std::ostream& sortie, Collider const& body) { // opérateur externe, permet d'afficher la position du centre et le rayon d'un Collider
     sortie << " Collider : position = " << body.getPosition() << " radius = " << body.getRadius() ;
     return sortie;
 }
--- Collider.hpp
+++ Collider.hpp
@@ -21,8 +21,8 @@ private :
 
 public :
     Collider (Vec2d position, double arg_radius); // constructeur
-    Collider& operator= (Collider const&) = default;
-    Collider (Collider const& autre); // constructeur de copie
+    Collider& operator= (Collider const&) = default; // opérateur =
+    Collider (Collider const& other); // constructeur de copie
 
     Vec2d clamp (const Vec2d& vec);
     Vec2d directionTo (const Vec2d& to) const;
@@ -43,5 +43,3 @@ public :
     Vec2d getPosition () const;
     double getRadius () const;
 };
-
-std::ostream& operator<< (std::ostream& sortie, Collider const& body);
